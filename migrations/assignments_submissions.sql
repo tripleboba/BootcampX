d562295bc5266cd879ce9744f9b25222f0e9cb34
Triple the Boba ! <coconuttt1010@gmail.com>
2022-02-10 03:39:18 -0800

    assignments tables added

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,17 @@
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id SERIAL REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id SERIAL REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_DATE DATE
+);
+
